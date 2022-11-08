d3da41e40d64d37e8647c27b29a3f9272c7c0b77
ZanderOl <olexandruk@gmail.com>
2022-11-08 18:48:06 +0200

    done with hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector(".gallery");
+const galleryList = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></div>`
+  )
+  .join("");
+
+// galleryDiv.innerHTML = galleryList;
+galleryDiv.insertAdjacentHTML("beforeend", galleryList);
+galleryDiv.addEventListener("click", openImg);
+
+let picture;
+
+function openImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName === "DIV") {
+    return;
+  }
+  picture = basicLightbox.create(`
+    <div class="modal">
+        <img src=${event.target.dataset.source} width="80%" style="margin: auto; display: block"> 
+    </div>
+`);
+
+  picture.show();
+  document.addEventListener("keydown", closeModal);
+}
+
+function closeModal(event) {
+  if (event.key === "Escape") {
+    picture.close();
+    document.removeEventListener("keydown", closeModal);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector(".gallery");
+
+const gallery = galleryItems
+  .map(
+    (img) =>
+      `<a class="gallery__item" href="${img.original}"><img class="gallery__image" src="${img.preview}" data-source="${img.original}" alt="${img.description}"/></a>`
+  )
+  .join("");
+
+galleryDiv.innerHTML = gallery;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
